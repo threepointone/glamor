7c15eba4714c2a18b78db2d9c25b2deb84e3b73a
Sunil Pai <threepointone@gmail.com>
2016-07-21 20:47:56 +0530

    dumb :|

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,16 +1,11 @@
 import React from 'react'
 
-import { style, hover } from '../../src'
-
-let red = style({ backgroundColor: 'red' }),
-  blue = style({ backgroundColor: 'blue' })
-
+import { hover } from '../../src'
 
 export class App extends React.Component {
   render() {
     return <div>
       This here is a link to
-      <div  {...red} {...blue} >sadasdasd</div>
       <a href='google.com'
         {...hover({ backgroundColor: 'red' })}
         // {...simulate('hover')}
