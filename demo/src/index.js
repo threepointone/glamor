346de3da77b2373e617cbeb4a2c7b398fa0bd8bb
Sunil Pai <threepointone@gmail.com>
2016-07-24 20:38:21 +0530

    react-hot-loader@3! for reals!

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,5 +1,20 @@
+import 'react-hot-loader/patch'
+
 import React from 'react' //eslint-disable-line
 import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader';
+
 import { App } from './app'
 
-render(<App/>, document.querySelector('#demo'))
+let make = App =>
+  <AppContainer><App/></AppContainer>
+
+render(make(App),
+  document.querySelector('#demo'))
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    render(make(require('./app').App),
+      document.querySelector('#demo'))
+  })
+}
