6cb56103ae11d8674cc7b28ea62570a7085c1b83
Sunil Pai <threepointone@gmail.com>
2016-08-03 09:11:57 +0530

    pass babel presets directly (#17)

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -10,7 +10,11 @@ module.exports = function (config) {
     },
     files: [ 'index.js' ],
     browserify: {
-      transform: [ 'babelify', 'envify' ]
+      transform: [ [
+        'babelify', {
+          presets: [ 'es2015', 'stage-0', 'react' ]
+        }
+      ], 'envify' ]
     }
   })
 }
