407a7c0dfa21daaf5a5cb3d73335d8b3de716199
Sunil Pai <threepointone@gmail.com>
2016-08-28 18:55:39 +0530

    er

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('rollup-plugin-babel')({
       presets: [ 'es2015-rollup', 'stage-0', 'react' ],
-      plugins: [ 'transform-decorators-legacy', 'external-helpers' ],
+      plugins: [ 'transform-decorators-legacy' ],
       exclude: 'node_modules/**'
     })
   ]
