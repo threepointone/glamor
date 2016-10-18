4e37cade00227a31e066b4120fccc399e014ef34
Sunil Pai <threepointone@gmail.com>
2016-10-18 19:34:08 +0530

    tighter build, <8k

--- webpack/umd.js
+++ webpack/umd.js
@@ -1,6 +1,6 @@
 let webpack = require('webpack')
 
-module.exports = {
+module.exports = {  
   entry: './src/index.js',
   output: {
     library: 'Glamor',
@@ -20,4 +20,16 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ]
+}
+
+if(process.env.NODE_ENV === 'production') {
+  module.exports.output.filename = 'index.min.js' 
+  module.exports.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    compress: true, mangle: true
+  })
+  )
+} 
+else {
+  module.exports.devtool = 'source-map'  
 }
