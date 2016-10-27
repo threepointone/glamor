6864818f442d79a18038129f13e8c015015595b0
Benjamin Otto <benjamin.otto@style-systems.de>
2016-10-27 11:45:02 +0200

    add support for propMerge() in react package

--- tests/index.js
+++ tests/index.js
@@ -25,6 +25,8 @@ import { style, hover, nthChild, firstLetter, media, merge, compose,  select, vi
   flush, styleSheet, rehydrate }
 from '../src'
 
+import { propMerge } from '../src/react'
+
 import clean from '../src/clean'
 
 import { View } from '../src/jsxstyle'
@@ -697,9 +699,41 @@ describe('plugins', () => {
 })
 
 describe('react', () => {
+  let node
+  beforeEach(() => {
+    node = document.createElement('div')
+    document.body.appendChild(node)
+  })
+
+  afterEach(() => {
+    unmountComponentAtNode(node)
+    document.body.removeChild(node)
+    flush()
+  })
+
   it('dom elements accept css prop')
   it('can use vars to set/unset values vertically on the dom-tree')
-  it('can use themes to compose styles vertically on  the dom-tree')  
+  it('can use themes to compose styles vertically on  the dom-tree')
+
+  it('propMerge will merge styles with react props', () => {
+    // the order of the styles matter here
+    // thats how css works :/
+    const specificStyle = style({ height: '100px' })
+    const defaultStyle = style({ height: '200px' })
+    const dummyStyle = style({ color: 'green' })
+
+    let DefaultContainer = (props) => <div {...propMerge(defaultStyle, props)}/>
+    let SpecificContainer = (props) => <DefaultContainer {...specificStyle} {...props}/>
+
+    render(<SpecificContainer/>, node, () => {
+      expect(childStyle(node).height).toEqual('100px')
+    })
+
+    // this would only work properly if we use propMerge also on <SpecificContainer/> definition
+    render(<SpecificContainer {...dummyStyle}/>, node, () => {
+      expect(childStyle(node).height).toEqual('200px')
+    })
+  })
 })
 
 describe('aphrodite', () => {
