77908b4adb68a97158a9f27f501b06a439f63d93
Sunil Pai <threepointone@gmail.com>
2016-11-15 18:33:37 +0530

    react example

--- examples/index.js
+++ examples/index.js
@@ -1,5 +1,5 @@
 import React from 'react' //eslint-disable-line
 import { render } from 'react-dom'
-import { App } from './tweet'
+import { App } from './react'
 render(<App/>,
   document.querySelector('#demo'))
