0bb5309d75f14af7de180ad12643dd94a0f4305d
Sunil Pai <threepointone@gmail.com>
2016-11-19 13:42:31 +0530

    storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,18 @@
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add addional webpack configurations.
+// For more information refer the docs: https://goo.gl/qPbSyX
+
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
+
+module.exports = {
+  plugins: [
+    // your custom plugins
+  ],
+  module: {
+    loaders: [
+      // add your custom loaders.
+    ],
+  },
+};
