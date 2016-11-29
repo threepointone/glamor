854cb27dc6b1622b225da4bea151dad3bf21fcd4
Sunil Pai <threepointone@gmail.com>
2016-11-30 04:21:48 +0530

    help out #94

--- src/index.js
+++ src/index.js
@@ -456,10 +456,15 @@ export function flush() {
 
 export const presets = {
   mobile : '(min-width: 400px)',
+  Mobile: '@media (min-width: 400px)',
   phablet : '(min-width: 550px)',
+  Phablet : '@media (min-width: 550px)',
   tablet : '(min-width: 750px)',
+  Tablet : '@media (min-width: 550px)',
   desktop : '(min-width: 1000px)',
-  hd : '(min-width: 1200px)'
+  Desktop : '@media (min-width: 1000px)',
+  hd : '(min-width: 1200px)',
+  Hd : '@media (min-width: 1200px)'
 }
 
 
