0941cd284766a45ff8fe3ca9b7e6d63299a49c73
Sunil Pai <threepointone@fb.com>
2017-07-21 16:33:07 +0100

    scope hoisting, lose es6 build

--- webpack/umd.js
+++ webpack/umd.js
@@ -21,7 +21,8 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
   ]
 }
 
