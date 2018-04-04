580058f67085e1cb6ad8d7cabe805b3e57d07507
Tom Crockett <pelotom@gmail.com>
2018-04-03 18:28:38 -0700

    Use csstype for a baseline of CSS definitions

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
-export interface CSSProperties {
+import * as CSS from 'csstype';
+
+export interface CSSProperties extends CSS.Properties<number | string> {
   /**
    * In dev mode, adding a `label` string prop will reflect its value in devtools. Useful
    * when debugging, and a good alternative to 'semantic' classnames.
    */
   label?: string;
-  // for now just allow everything
+  // for now just allow everything as a fallback
   [propertyName: string]: any;
 }
 
