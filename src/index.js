9a4d28677e156ee755143557073f27cc28b889b4
Stef Busking <stef.busking@gmail.com>
2023-12-20 11:58:43 +0100

    Remove more unused API

--- src/index.js
+++ src/index.js
@@ -10,11 +10,6 @@ export const styleSheet = new StyleSheet();
 styleSheet.inject(); //eslint-disable-line indent
 // /****************      TO THE MOOOOOOON     ****************/
 
-// convenience function to toggle speedy
-export function speedy(bool) {
-  return styleSheet.speedy(bool);
-}
-
 // plugins
 import { PluginSet, fallbacks, contentWrap } from "./plugins"; // we include these by default
 export const plugins = (styleSheet.plugins = new PluginSet([
@@ -27,65 +22,7 @@ plugins.keyframes = new PluginSet([fallbacks]);
 
 // define some constants
 
-const isDev = false;
-const isTest = false;
 const isBrowser = typeof window !== "undefined";
-
-/**** simulations  ****/
-
-// a flag to enable simulation meta tags on dom nodes
-// defaults to true in dev mode. recommend *not* to
-// toggle often.
-let canSimulate = isDev;
-
-// we use these flags for issuing warnings when simulate is called
-// in prod / in incorrect order
-let warned1 = false,
-  warned2 = false;
-
-// toggles simulation activity. shouldn't be needed in most cases
-export function simulations(bool = true) {
-  canSimulate = !!bool;
-}
-
-// use this on dom nodes to 'simulate' pseudoclasses
-// <div {...hover({ color: 'red' })} {...simulate('hover', 'visited')}>...</div>
-// you can even send in some weird ones, as long as it's in simple format
-// and matches an existing rule on the element
-// eg simulate('nthChild2', ':hover:active') etc
-export function simulate(...pseudos) {
-  pseudos = clean(pseudos);
-  if (!pseudos) return {};
-  if (!canSimulate) {
-    if (!warned1) {
-      console.warn("can't simulate without once calling simulations(true)"); //eslint-disable-line no-console
-      warned1 = true;
-    }
-    if (!isDev && !isTest && !warned2) {
-      console.warn("don't use simulation outside dev"); //eslint-disable-line no-console
-      warned2 = true;
-    }
-    return {};
-  }
-  return pseudos.reduce(
-    (o, p) => ((o[`data-simulate-${simple(p)}`] = ""), o),
-    {}
-  );
-}
-
-/**** labels ****/
-// toggle for debug labels.
-// *shouldn't* have to mess with this manually
-let hasLabels = isDev;
-
-export function cssLabels(bool) {
-  hasLabels = !!bool;
-}
-
-// takes a string, converts to lowercase, strips out nonalphanumeric.
-function simple(str, char = "") {
-  return str.toLowerCase().replace(/[^a-z0-9]/g, char);
-}
 
 // hashes a string to something 'unique'
 // we use this to generate ids for styles
@@ -94,14 +31,11 @@ import hash from "./hash";
 function hashify(obj) {
   let str = JSON.stringify(obj);
   let toRet = hash(str).toString(36);
-  if (obj.label && obj.label.length > 0 && isDev) {
-    return simple(obj.label.join("."), "-") + "-" + toRet;
-  }
   return toRet;
 }
 
 // of shape { 'data-css-<id>': '' }
-export function isLikeRule(rule) {
+function isLikeRule(rule) {
   let keys = Object.keys(rule).filter((x) => x !== "toString");
   if (keys.length !== 1) {
     return false;
@@ -110,7 +44,7 @@ export function isLikeRule(rule) {
 }
 
 // extracts id from a { 'data-css-<id>': ''} like object
-export function idFor(rule) {
+function idFor(rule) {
   let keys = Object.keys(rule).filter((x) => x !== "toString");
   if (keys.length !== 1) throw new Error("not a rule");
   let regex = /data\-css\-([a-zA-Z0-9\-_]+)/;
@@ -186,11 +120,6 @@ function selector(id, path) {
     )
     .join(",");
 
-  if (canSimulate && /^\&\:/.exec(path) && !/\s/.exec(path)) {
-    x += `,.css-${id}[data-simulate-${simple(
-      path
-    )}],[data-css-${id}][data-simulate-${simple(path)}]`;
-  }
   return x;
 }
 
@@ -217,7 +146,6 @@ function deconstruct(style) {
     } else if (key === "label") {
       if (style.label.length > 0) {
         plain = plain || {};
-        plain.label = hasLabels ? style.label.join(".") : "";
       }
     } else {
       plain = plain || {};
@@ -299,7 +227,7 @@ function toRule(spec) {
     return ruleCache[spec.id];
   }
 
-  let ret = { [`data-css-${spec.id}`]: hasLabels ? spec.label || "" : "" };
+  let ret = { [`data-css-${spec.id}`]: "" };
   Object.defineProperty(ret, "toString", {
     enumerable: false,
     value() {
@@ -441,9 +369,6 @@ function build(dest, { selector = "", mq = "", supp = "", src = {} }) {
         }
 
         if (key === "label") {
-          if (hasLabels) {
-            dest.label = dest.label.concat(_src.label);
-          }
         } else {
           _dest[key] = _src[key];
         }
@@ -459,7 +384,7 @@ function _css(rules) {
   let spec = {
     id: hashify(style),
     style,
-    label: hasLabels ? style.label.join(".") : "",
+    label: "",
     type: "css",
   };
   return toRule(spec);
@@ -480,7 +405,6 @@ let inputCaches =
     ? [nullrule, new WeakMap(), new WeakMap(), new WeakMap()]
     : [nullrule];
 
-let warnedWeakMapError = false;
 function multiIndexCache(fn) {
   return function (args) {
     if (inputCaches[args.length]) {
@@ -512,15 +436,7 @@ function multiIndexCache(fn) {
       }
       try {
         coi.set(args[ctr], value);
-      } catch (err) {
-        if (isDev && !warnedWeakMapError) {
-          warnedWeakMapError = true;
-          console.warn("failed setting the WeakMap cache for args:", ...args); // eslint-disable-line no-console
-          console.warn(
-            "this should NOT happen, please file a bug on the github repo."
-          ); // eslint-disable-line no-console
-        }
-      }
+      } catch (err) {}
     }
     return value;
   };
@@ -543,7 +459,7 @@ export function css(...rules) {
   return cachedCss(rules);
 }
 
-css.insert = (css) => {
+function insertCss(css) {
   let spec = {
     id: hashify(css),
     css,
@@ -554,18 +470,14 @@ css.insert = (css) => {
     styleSheet.insert(spec.css);
     inserted[spec.id] = isBrowser ? true : [spec.css];
   }
-};
-
-export const insertRule = css.insert;
+}
 
 css.global = (selector, style) => {
   style = clean(style);
   if (style) {
-    return css.insert(toCSS({ selector, style }));
+    return insertCss(toCSS({ selector, style }));
   }
 };
-
-export const insertGlobal = css.global;
 
 function insertKeyframe(spec) {
   if (!inserted[spec.id]) {
@@ -588,7 +500,7 @@ function insertKeyframe(spec) {
     inserted[spec.id] = isBrowser ? true : rules;
   }
 }
-css.keyframes = (name, kfs) => {
+export const keyframes = (name, kfs) => {
   if (!kfs) {
     (kfs = name), (name = "animation");
   }
@@ -605,313 +517,3 @@ css.keyframes = (name, kfs) => {
   insertKeyframe(spec);
   return name + "_" + spec.id;
 };
-
-// we don't go all out for fonts as much, giving a simple font loading strategy
-// use a fancier lib if you need moar power
-css.fontFace = (font) => {
-  font = clean(font);
-  let spec = {
-    id: hashify(font),
-    type: "font-face",
-    font,
-  };
-  register(spec);
-  insertFontFace(spec);
-
-  return font.fontFamily;
-};
-
-export const fontFace = css.fontFace;
-export const keyframes = css.keyframes;
-
-function insertFontFace(spec) {
-  if (!inserted[spec.id]) {
-    const rule = `@font-face{${createMarkupForStyles(spec.font)}}`;
-    styleSheet.insert(rule);
-    inserted[spec.id] = isBrowser ? true : [rule];
-  }
-}
-
-// rehydrate the insertion cache with ids sent from
-// renderStatic / renderStaticOptimized
-export function rehydrate(ids) {
-  // load up ids
-  Object.assign(
-    inserted,
-    ids.reduce((o, i) => ((o[i] = true), o), {})
-  );
-  // assume css loaded separately
-}
-
-// clears out the cache and empties the stylesheet
-// best for tests, though there might be some value for SSR.
-
-export function flush() {
-  inserted = styleSheet.inserted = {};
-  registered = styleSheet.registered = {};
-  ruleCache = {};
-  styleSheet.flush();
-  styleSheet.inject();
-}
-
-export const presets = {
-  mobile: "(min-width: 400px)",
-  Mobile: "@media (min-width: 400px)",
-  phablet: "(min-width: 550px)",
-  Phablet: "@media (min-width: 550px)",
-  tablet: "(min-width: 750px)",
-  Tablet: "@media (min-width: 750px)",
-  desktop: "(min-width: 1000px)",
-  Desktop: "@media (min-width: 1000px)",
-  hd: "(min-width: 1200px)",
-  Hd: "@media (min-width: 1200px)",
-};
-
-export const style = css;
-
-export function select(selector, ...styles) {
-  if (!selector) {
-    return style(styles);
-  }
-  return css({ [selector]: styles });
-}
-export const $ = select;
-
-export function parent(selector, ...styles) {
-  return css({ [`${selector} &`]: styles });
-}
-
-export const merge = css;
-export const compose = css;
-
-export function media(query, ...rules) {
-  return css({ [`@media ${query}`]: rules });
-}
-
-export function pseudo(selector, ...styles) {
-  return css({ [selector]: styles });
-}
-
-// allllll the pseudoclasses
-
-export function active(x) {
-  return pseudo(":active", x);
-}
-
-export function any(x) {
-  return pseudo(":any", x);
-}
-
-export function checked(x) {
-  return pseudo(":checked", x);
-}
-
-export function disabled(x) {
-  return pseudo(":disabled", x);
-}
-
-export function empty(x) {
-  return pseudo(":empty", x);
-}
-
-export function enabled(x) {
-  return pseudo(":enabled", x);
-}
-
-export function _default(x) {
-  return pseudo(":default", x); // note '_default' name
-}
-
-export function first(x) {
-  return pseudo(":first", x);
-}
-
-export function firstChild(x) {
-  return pseudo(":first-child", x);
-}
-
-export function firstOfType(x) {
-  return pseudo(":first-of-type", x);
-}
-
-export function fullscreen(x) {
-  return pseudo(":fullscreen", x);
-}
-
-export function focus(x) {
-  return pseudo(":focus", x);
-}
-
-export function hover(x) {
-  return pseudo(":hover", x);
-}
-
-export function indeterminate(x) {
-  return pseudo(":indeterminate", x);
-}
-
-export function inRange(x) {
-  return pseudo(":in-range", x);
-}
-
-export function invalid(x) {
-  return pseudo(":invalid", x);
-}
-
-export function lastChild(x) {
-  return pseudo(":last-child", x);
-}
-
-export function lastOfType(x) {
-  return pseudo(":last-of-type", x);
-}
-
-export function left(x) {
-  return pseudo(":left", x);
-}
-
-export function link(x) {
-  return pseudo(":link", x);
-}
-
-export function onlyChild(x) {
-  return pseudo(":only-child", x);
-}
-
-export function onlyOfType(x) {
-  return pseudo(":only-of-type", x);
-}
-
-export function optional(x) {
-  return pseudo(":optional", x);
-}
-
-export function outOfRange(x) {
-  return pseudo(":out-of-range", x);
-}
-
-export function readOnly(x) {
-  return pseudo(":read-only", x);
-}
-
-export function readWrite(x) {
-  return pseudo(":read-write", x);
-}
-
-export function required(x) {
-  return pseudo(":required", x);
-}
-
-export function right(x) {
-  return pseudo(":right", x);
-}
-
-export function root(x) {
-  return pseudo(":root", x);
-}
-
-export function scope(x) {
-  return pseudo(":scope", x);
-}
-
-export function target(x) {
-  return pseudo(":target", x);
-}
-
-export function valid(x) {
-  return pseudo(":valid", x);
-}
-
-export function visited(x) {
-  return pseudo(":visited", x);
-}
-
-// parameterized pseudoclasses
-export function dir(p, x) {
-  return pseudo(`:dir(${p})`, x);
-}
-export function lang(p, x) {
-  return pseudo(`:lang(${p})`, x);
-}
-export function not(p, x) {
-  // should this be a plugin?
-  let selector = p
-    .split(",")
-    .map((x) => x.trim())
-    .map((x) => `:not(${x})`);
-  if (selector.length === 1) {
-    return pseudo(`:not(${p})`, x);
-  }
-  return select(selector.join(""), x);
-}
-export function nthChild(p, x) {
-  return pseudo(`:nth-child(${p})`, x);
-}
-export function nthLastChild(p, x) {
-  return pseudo(`:nth-last-child(${p})`, x);
-}
-export function nthLastOfType(p, x) {
-  return pseudo(`:nth-last-of-type(${p})`, x);
-}
-export function nthOfType(p, x) {
-  return pseudo(`:nth-of-type(${p})`, x);
-}
-
-// pseudoelements
-export function after(x) {
-  return pseudo("::after", x);
-}
-export function before(x) {
-  return pseudo("::before", x);
-}
-export function firstLetter(x) {
-  return pseudo("::first-letter", x);
-}
-export function firstLine(x) {
-  return pseudo("::first-line", x);
-}
-export function selection(x) {
-  return pseudo("::selection", x);
-}
-export function backdrop(x) {
-  return pseudo("::backdrop", x);
-}
-export function placeholder(x) {
-  // https://github.com/threepointone/glamor/issues/14
-  return css({ "::placeholder": x });
-}
-
-/*** helpers for web components ***/
-// https://github.com/threepointone/glamor/issues/16
-
-export function cssFor(...rules) {
-  rules = clean(rules);
-  return rules
-    ? rules
-        .map((r) => {
-          let style = { label: [] };
-          build(style, { src: r }); // mutative! but worth it.
-          return deconstructedStyleToCSS(
-            hashify(style),
-            deconstruct(style)
-          ).join("");
-        })
-        .join("")
-    : "";
-}
-
-export function attribsFor(...rules) {
-  rules = clean(rules);
-  let htmlAttributes = rules
-    ? rules
-        .map((rule) => {
-          idFor(rule); // throwaway check for rule
-          let key = Object.keys(rule)[0],
-            value = rule[key];
-          return `${key}="${value || ""}"`;
-        })
-        .join(" ")
-    : "";
-
-  return htmlAttributes;
-}
--- webpack/dev.js
+++ webpack/dev.js
@@ -1,38 +1,33 @@
-let webpack = require('webpack')
-let path = require('path')
+let webpack = require("webpack");
+let path = require("path");
 module.exports = {
-  entry: './examples/index.js',
+  entry: "./examples/index.js",
   output: {
-    path: path.resolve('./examples'),
-    filename: 'bundle.js'
+    path: path.resolve("./examples"),
+    filename: "bundle.js",
   },
   module: {
-    rules: [ {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        plugins: [ 
-          path.join(__dirname, '../src/css/babel.js')
-        ]
-      }
-    } ]  
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        query: {
+          plugins: [path.join(__dirname, "../src/css/babel.js")],
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
-      'glamor': '../src'
-    }
+      glamor: "../src",
+    },
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
-  ],
   devServer: {
-    stats: 'errors-only',
-    contentBase: 'examples/',
+    stats: "errors-only",
+    contentBase: "examples/",
     historyApiFallback: true,
-    compress: true, 
-    inline: true
-  }
-}
+    compress: true,
+    inline: true,
+  },
+};
